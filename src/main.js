d8eba7e3eae8bd23d8a9079cbae577a898a950c3
lightors <fenganhua@foxmail.com>
2020-03-16 22:29:13 +0800

    vant

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from "./router";
 
+import { Button, Skeleton ,Image,Lazyload,Grid, GridItem,Card,SubmitBar,Sidebar, SidebarItem } from 'vant';
+Vue.use(Image);
+Vue.use(Button);
+Vue.use(Skeleton);
+Vue.use(Lazyload);
+Vue.use(Grid);
+Vue.use(GridItem);
+Vue.use(Card);
+Vue.use(SubmitBar);
+Vue.use(Sidebar);
+Vue.use(SidebarItem);
+
 
 Vue.config.productionTip = false
 
--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,10 +1,28 @@
 <template>
-  <h2>购物车</h2>
+  <scroll>
+  <!--  <div style="position: absolute;top: 0;">
+      <van-submit-bar
+        safe-area-inset-bottom="true"
+        :price="3050"
+        button-text="提交订单"
+        @submit="onSubmit"
+      >
+        <van-checkbox v-model="checked">全选</van-checkbox>
+        <span slot="tip">
+    你的收货地址不支持同城送, <span>修改地址</span>
+  </span>
+      </van-submit-bar>
+    </div>-->
+  </Scroll>
 </template>
 
 <script>
+  import Scroll from "../../components/common/scroll/Scroll";
   export default {
-    name: "Cart"
+    name: "Cart",
+    components: {
+      Scroll,
+    }
   }
 </script>
 
